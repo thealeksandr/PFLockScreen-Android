a3e6cef10f3e18432107a4a1568e29f1ec2ca92b
Aleksandr Nikiforov <i.thealeksandr@gmail.com>
2018-02-26 23:14:26 +0700

    Added test for PFFingerprintPinCodeHelper

--- pflockscreen/src/androidTest/java/com/beautycoder/pflockscreen/PFSecurityUtilsTest.java
+++ pflockscreen/src/androidTest/java/com/beautycoder/pflockscreen/PFSecurityUtilsTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.beautycoder.pflockscreen.security.PFFingerprintPinCodeHelper;
 import com.beautycoder.pflockscreen.security.PFSecurityUtils;
 
 import org.junit.Test;
@@ -20,7 +21,7 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class PFSecurityUtilsTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void pfSecurityUtils() throws Exception {
         // Context of the app under test.
         String alias = "test_alias";
         String pinCode = "1234";
@@ -46,4 +47,37 @@ public class PFSecurityUtilsTest {
         assertFalse(isAliasFalse2);
 
     }
+
+
+    @Test
+    public void pfFingerPrintPinCodeHelper() throws Exception {
+        // Context of the app under test.
+        String pinCode = "1234";
+
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        PFFingerprintPinCodeHelper.getInstance().delete();
+
+        boolean isAliasFalse = PFFingerprintPinCodeHelper.getInstance().isPinCodeEncryptionKeyExist();
+        assertFalse(isAliasFalse);
+
+        String encoded = PFFingerprintPinCodeHelper.getInstance().encodePin(appContext, pinCode, false);
+        assertNotNull(encoded);
+
+        boolean isAliasTrue = PFFingerprintPinCodeHelper.getInstance().isPinCodeEncryptionKeyExist();
+        assertTrue(isAliasTrue);
+
+        boolean isCorrect = PFFingerprintPinCodeHelper.getInstance().checkPin(appContext, encoded, pinCode);
+        assertTrue(isCorrect);
+
+        boolean isWrong = PFFingerprintPinCodeHelper.getInstance().checkPin(appContext, encoded, "1122");
+        assertFalse(isWrong);
+
+        PFFingerprintPinCodeHelper.getInstance().delete();
+
+        boolean isAliasFalse2 = PFFingerprintPinCodeHelper.getInstance().isPinCodeEncryptionKeyExist();
+        assertFalse(isAliasFalse2);
+
+    }
+
 }
