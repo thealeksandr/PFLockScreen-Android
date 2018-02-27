b0bf2c3876dd1faea5959b37796ce9bb97f5c1d1
Aleksandr Nikiforov <i.thealeksandr@gmail.com>
2018-02-27 14:34:08 +0700

    Added Test rule and TestActivity for independent fragment testing.

---
+++ pflockscreen/src/androidTest/java/com/beautycoder/pflockscreen/FragmentTestRule.java
@@ -0,0 +1,57 @@
+package com.beautycoder.pflockscreen;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.beautycoder.pflockscreen.activities.TestActivity;
+
+import junit.framework.Assert;
+
+/**
+ * Created by Aleksandr Nikiforov on 2018/02/27.
+ */
+
+public class FragmentTestRule <F extends Fragment> extends ActivityTestRule<TestActivity> {
+
+    private final Class<F> mFragmentClass;
+    private F mFragment;
+
+    public FragmentTestRule(final Class<F> fragmentClass) {
+        super(TestActivity.class, true, false);
+        mFragmentClass = fragmentClass;
+    }
+
+    @Override
+    protected void afterActivityLaunched() {
+        super.afterActivityLaunched();
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //Instantiate and insert the fragment into the container layout
+                    FragmentManager manager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    mFragment = mFragmentClass.newInstance();
+                    transaction.replace(android.R.id.content, mFragment);
+                    transaction.commit();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    Assert.fail(String.format("%s: Could not insert %s into TestActivity: %s",
+                            getClass().getSimpleName(),
+                            mFragmentClass.getSimpleName(),
+                            e.getMessage()));
+                }
+            }
+        });
+    }
+
+
+    public F getFragment(){
+        return mFragment;
+    }
+
+
+}
